4580b08aaf0079b625d1e6917940aa2490318cfc
RealAnalysis <RealAnalysis@RealAnalysiss-iMac.local>
2020-07-18 19:28:33 +0800

    select box

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,7 @@ class Post < ApplicationRecord
   has_many :liked_users, :through => :likes, :source => :user
   has_many :favorites, :dependent => :destroy
   has_many :favorite_posts, :through => :favorites, :source => :user
+  belongs_to :category, :optional => true
 
   def find_like(user)
     self.likes.where( :user_id => user.id ).first
